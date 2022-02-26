1f889d72bd596d97149a9dbc8280217e38e1ba98
OrShemesh5020 <orshemesh5020@gmail.com>
2022-02-26 11:24:06 +0200

    adding the menu molecule

---
+++ components/atoms/SearchInput/SearchInput.tsx
@@ -0,0 +1,7 @@
+import styles from './SearchInput.module.css'
+const SearchInput=()=>{
+return(
+        <input className={styles.input} placeholder="search"></input>
+);
+}
+export default SearchInput;
