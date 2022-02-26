e4a653597d86369e8fefa3b75550e642ba23ab44
OrShemesh5020 <orshemesh5020@gmail.com>
2022-02-26 11:32:03 +0200

    adding header to the home page

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,8 +1,11 @@
 import { NextPage } from "next"
+import Header from "../components/organisms/Header/Header"
 import Head from "next/head";
 
 const Home: NextPage = () => {
 return(
-    <div>home page</div>
+    <>
+        <div><Header/></div>
+    </>    
 )}
 export default Home;
