d3197f8327a85de84f2cb77b17da74fad2913794
or shemesh <orshemesh5020@gmail.com>
2022-05-17 12:26:13 +0300

    adding a mock for the search bar

---
+++ mock/SearchResults.js
@@ -0,0 +1,48 @@
+const SearchResults=[
+    {
+        id: 1,
+        title: 'aaa'
+    },
+    {
+        id: 2,
+        title: 'ababbb'
+    },
+    {
+        id: 3,
+        title: 'aacbbb'
+    },
+    {
+        id: 4,
+        title: 'acabbb'
+    },
+    {
+        id: 5,
+        title: 'abcdeb'
+    },
+    {
+        id: 6,
+        title: 'gfdfdn'
+    },
+    {
+        id: 7,
+        title: 'sdvsd'
+    },
+    {
+        id: 8,
+        title: 'rgew'
+    },
+    {
+        id: 9,
+        title: 'dfbn'
+    },
+    {
+        id: 10,
+        title: 'esvd'
+    },
+    {
+        id: 11,
+        title: 'nfgng'
+    },
+    
+]
+export default SearchResults;
