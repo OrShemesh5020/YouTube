d2b1bff38344cc7ae3b9f7d3e429349052fc6743
or shemesh <orshemesh5020@gmail.com>
2022-05-17 12:25:12 +0300

    refactoring

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,7 +1,13 @@
 import { NextPage } from "next"
-
+import Link from "next/link";
+import SearchResults from "../mock/SearchResults";
 const search: NextPage = () => {
 return(
-    <div>search page</div>
+    <>
+    <div>search page:</div>
+    </>
 )}
+
+
+
 export default search;
