d170dede62186bba89a85796e39c55fe344f2555
or shemesh <orshemesh5020@gmail.com>
2022-05-17 12:28:02 +0300

    Search bar filter fetcher

--- components/molecules/Search/Search.tsx
+++ components/molecules/Search/Search.tsx
@@ -1,11 +1,25 @@
+import React,{useState} from 'react';
 import Input from '../../atoms/SearchInput/SearchInput';
 import Button from '../../atoms/SearchButton/SearchButton';
 import styles from './Search.module.css';
+import SearchResults from '../../../mock/SearchResults';
 const Search = () => {
+  const [filteredData,setFilteredData]=useState([]);
+  const handleFilter=(event)=>{
+    const searchWord= event.target.value
+    console.log(searchWord);
+  }
   return (
     <div className={styles.wrapper}>
-        <Input className={styles.input} />
-         <Button />
+      <Input className={styles.input} />
+      <Button />
+      {filteredData.length!=0 &&(
+       <div className={styles.dataResult}>
+         {SearchResults.map((val,key)=>{
+         return <div className={styles.dataItem}>{val.title}</div>;
+         })}
+       </div>
+       )}
     </div>
   );
 };
