0582bae73f239b0d38b152bec6ec5d5f65403426
or shemesh <orshemesh5020@gmail.com>
2022-05-25 00:30:26 +0300

    creating a sub menu

--- components/atoms/HamburgerMenu/HamburgerMenu.tsx
+++ components/atoms/HamburgerMenu/HamburgerMenu.tsx
@@ -1,13 +1,10 @@
 import Hamburger from './HamburgerMenu.png'
 import Image from 'next/image'
 import styles from './HamburgerMenu.module.css'
-const HamburgerMenu=()=>{
+const HamburgerMenu=({onClick}:{onCkick:Function})=>{
 return(
-     <div className={styles.menu}><Image  onClick={clicked} src={Hamburger} alt="Hamburger menu"></Image></div>
+     <div className={styles.menu}><Image onClick={onClick} src={Hamburger} alt="Hamburger menu"></Image></div>
 );
 }
 
-function clicked() {
-     console.log('you clicked on the Hamburger menu');
-}
 export default HamburgerMenu;
