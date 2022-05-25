1b23f52fe9b03e5e59e67a21679269de55b46455
or shemesh <orshemesh5020@gmail.com>
2022-05-26 00:13:06 +0300

    adding profile menu

---
+++ data/profileMenu.ts
@@ -0,0 +1,30 @@
+import ProfileMenu from '../types/profileMenu'
+const ProfileMenu: ProfileMenu = [
+    {
+        id: 1,
+        title: 'My channel',
+        href: '/channel'
+    },
+    {
+        id: 2,
+        title: 'change account',
+        href: '/home'
+    },
+    {
+        id: 3,
+        title: 'Setting',
+        href: '/posts'
+    },
+    {
+        id: 4,
+        title: 'Help',
+        href: '/about'
+    },
+    {
+        id: 5,
+        title: 'Log out',
+        href: '/'
+    },
+
+]
+export default ProfileMenu
