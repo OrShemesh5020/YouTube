241102f33bbd7fdaf6911b91ad0ab9f2fed61a85
Eliran Shemesh <eliran@steps.me>
2022-05-26 11:32:28 +0300

    wip

--- components/atoms/SearchInput/SearchInput.tsx
+++ components/atoms/SearchInput/SearchInput.tsx
@@ -2,9 +2,12 @@ import styles from './SearchInput.module.css'
 import classNames from 'classnames'
 import SearchResults from './../../../mock/SearchResults';
 import handleFilter from '../../molecules/Search/Search'
-const SearchInput=({className}:{className:object})=>{
-return(
-        <input className={classNames(styles.input,className)} placeholder="Search..." onChange={handleFilter}></input>
-);
+
+
+const SearchInput = ({ className }: { className: object }) => {
+        return (
+                <input className={classNames(styles.input, className)} placeholder="Search..." onChange={handleFilter}></input>
+        );
 }
+
 export default SearchInput;
--- components/molecules/Search/Search.tsx
+++ components/molecules/Search/Search.tsx
@@ -1,25 +1,30 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import Input from '../../atoms/SearchInput/SearchInput';
 import Button from '../../atoms/SearchButton/SearchButton';
 import styles from './Search.module.css';
 import SearchResults from '../../../mock/SearchResults';
 const Search = () => {
-  const [filteredData,setFilteredData]=useState([]);
-  const handleFilter=(event)=>{
-    const searchWord= event.target.value
+  const [filteredData, setFilteredData] = useState([]);
+  const handleFilter = (event) => {
+    const searchWord = event.target.value
     console.log(searchWord);
   }
   return (
     <div className={styles.wrapper}>
       <Input className={styles.input} />
       <Button />
-      {filteredData.length!=0 &&(
-       <div className={styles.dataResult}>
-         {SearchResults.map((val,key)=>{
-         return <div className={styles.dataItem}>{val.title}</div>;
-         })}
-       </div>
-       )}
+      {filteredData.length != 0 && (
+        <div className={styles.dataResult}>
+
+
+
+          {SearchResults.map((val, key) => {
+          
+          
+          return <div className={styles.dataItem}>{val.title}</div>;
+          })}
+        </div>
+      )}
     </div>
   );
 };
