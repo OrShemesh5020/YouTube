b57e86331aee2ec255f3a814b1f116903c53e86a
or shemesh <orshemesh5020@gmail.com>
2022-06-23 16:31:31 +0300

    changing the hamburger menu icon

--- components/atoms/HamburgerMenu/HamburgerMenu.tsx
+++ components/atoms/HamburgerMenu/HamburgerMenu.tsx
@@ -1,7 +1,7 @@
 import Hamburger from './HamburgerMenu.png'
 import Image from 'next/image'
 import styles from './HamburgerMenu.module.css'
-const HamburgerMenu=({onClick}:{onCkick:Function})=>{
+const HamburgerMenu=({onClick}:{onCkick:string})=>{
 return(
      <div className={styles.menu}><Image onClick={onClick} src={Hamburger} alt="Hamburger menu"></Image></div>
 );
